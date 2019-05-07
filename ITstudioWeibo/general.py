a2a25e1ff961032e2e24ae20c2f66ea1cf2d8cf1
CalunVier <CalunVier@outlook.com>
2019-05-07 19:12:24 +0800

    电子邮件加入use标识

--- ITstudioWeibo/general.py
+++ ITstudioWeibo/general.py
@@ -7,13 +7,13 @@ import time
 import _thread
 import logging
 
-logger = logging.getLogger('django.general')
+logger = logging.getLogger('my_logger.general')
 
 
-def check_email_verify_code_not_right(verify_id, verify_code):
+def check_email_verify_code_not_right(verify_id, verify_code, use):
     vc = cache.get(verify_id, {})
     if vc:
-        if verify_code == vc['code']:
+        if verify_code == vc.get('code', '') and use == vc.get('use', ''):
             return 0
         else:
             return 1
@@ -21,27 +21,28 @@ def check_email_verify_code_not_right(verify_id, verify_code):
         return 2
 
 
-def to_send_email_verify_code(to_email):
+def to_send_email_verify_code(to_email, use):
 
     # 生成验证码
     code = str(random.randint(100000, 999999))
     logger.debug("验证码生成")
     # 发送邮件部分
     send_mail(
-        'ITStudio Mtime A组 验证码',
+        'ITStudio 微博 验证码',
         '您的验证码为：'+code+",验证码在60分钟内有效（请勿回复）",
         'itstudiomtimea@163.com',
         [to_email],
         fail_silently=False,
     )
     logger.debug('邮件发送成功')
-    cache.set(to_email, {'sent_time': time.time(), 'code': code}, 3600)
+    cache.set(to_email, {'sent_time': time.time(), 'code': code, 'use': use}, 3600)
 
 
 def i_get_email_verify_code(request):
     if request.method == 'GET':
         logger.debug('收到GET请求')
         email = request.GET.get('email', '')
+        use = request.GET.get('use', '')
         # 检查邮箱是否为空
         if check_email_verify(email):
 
@@ -52,13 +53,13 @@ def i_get_email_verify_code(request):
                     return HttpResponse("{\"status\":\"too_fast\"}")
 
             # 满足条件发送邮件
-            _thread.start_new_thread(to_send_email_verify_code, (email,))
+            _thread.start_new_thread(to_send_email_verify_code, (email, use,))
             logger.debug("创建新线程发送邮件")
             return HttpResponse("{\"id\":\""+email+"\",\"wait\":60,\"status\":\"ok\"}")
         else:
             # 邮箱不合法
-            logger.info("邮箱不合法")
-            return HttpResponse("{\"status\":\"invalid_email\"}")
+            logger.info("邮箱不合法:%s" % email)
+            return HttpResponse("{\"status\":\"invalid_email\"}", status=412)
     else:
         logger.info('收到非GET请求')
         return HttpResponse(status=404)
